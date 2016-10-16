be9c51b4af458df1d78a13ec05190cf4436518d2
sbmettu <smettu@ufl.edu>
2016-10-16 19:04:56 -0400

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,41 @@
+## The following two functions work individually to store the matrix and 
+## caches its inverse after inputting a matrix whose inverse is to be found
+
+## makeCacheMatrix is the function that creates a special "matrix" that can cache
+## its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## cacheSolve function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
+## tips for solving in the console:enter a diagonal matrix and then call 
+## the matrix through makeCacheMatrix and cacheSolve to finally get the inverse
+
